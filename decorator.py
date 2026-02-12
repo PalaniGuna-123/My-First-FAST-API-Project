6e6c788299e7bfbdbc579508b46a5ced022e6dd6
PalaniGuna-123 <gunaguna20994@gmail.com>
2026-02-12 11:33:54 +0530

    learn decorator with pizza  exmple

--- decorator.py
+++ decorator.py
@@ -1,4 +1,18 @@
-def wrapper():
-    print("Preparing a pizza base...   ")
+# def wrapper():
+#     print("Preparing a pizza base...   ")
 
-wrapper()
+# wrapper()
+
+def my_decorator(func):
+    def wrapper():
+        print("Preparing a pizza base...   ")
+        func()
+        print("Adding toppings...   ")
+
+    return wrapper
+
+@my_decorator
+def make_pizza():   
+    print("Baking the pizza...   ") 
+
+make_pizza()
