5df7d6acf3c7a102d05938107a07ed091dcfee16
PalaniGuna-123 <gunaguna20994@gmail.com>
2026-02-12 11:25:38 +0530

    implemented name decorator

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "This is my first FastAPI application!"}
+
+@app.get("/name")
+def read_name():
+    return {"message": "My name is FastAPI!"}
