037e450f2592b3dd175fb7015ee3cd863b563fad
PalaniGuna-123 <gunaguna20994@gmail.com>
2026-02-18 12:02:23 +0530

    complete recommend get API

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return {"message": "This is my first FastAPI application!"}
+
+# @app.get("/name")
+# def read_name():
+#     return {"message": "My name is FastAPI!"}
+
 from fastapi import FastAPI
 
 app = FastAPI()
+@app.get("/recommend")
+def recommend_items(age:int , interest:str):
+    '''
+    Example: GET /recommend?age=25&interest=technology
+    '''
+    if age < 18:
+        category="Teenagers"
+    elif age < 40:
+        category="Adults"
+    else:
+        category="Seniors"
 
-@app.get("/")
-def read_root():
-    return {"message": "This is my first FastAPI application!"}
-
-@app.get("/name")
-def read_name():
-    return {"message": "My name is FastAPI!"}
+    recommendations =[]
+    if interest.lower()=="music":
+        recommendations =["Spotify Playlist", "Apple Music", "YouTube Music", "concert tickets"]
+    elif interest.lower()=="Sports":
+        recommendations =["Nike Shoes", "Adidas Apparel", "Sports Equipment", "Fitness Tracker", "Gym Membership","Running Shoes"]
+    elif interest.lower()=="books":
+        recommendations =["Fiction Novels", "Non-fiction Books", "E-books", "Audiobooks", "Bookstore Gift Card"]
+    else:
+        recommendations=["Gift Card", "Subscription Box", "Experience Gift", "Personalized Item"]
+    return {"category": category, "recommendations": recommendations}
+    
+    
